3b228f70f3659715a8670d9f6277c12c1726b50a
Jeswin-J <jeswinjoseph164@gmail.com>
2023-08-06 21:14:48 +0530

    Updations

--- SymptomsChecker/checker/views.py
+++ SymptomsChecker/checker/views.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render, redirect
-import requests
+from .utils import WebDoctor
 
 import json
+
+doctor = WebDoctor()
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+
 def basic_info(request):
     return render(request, 'basic_info.html')
+
 
 def symptoms(request):
     return render(request, 'symptoms.html')
 
+
 def questions(request):
     return render(request, 'questions.html')
 
-def conditions(request):
-    return render(request, 'conditions.html')
+
+def conditions(request, disease, predictions, symptoms):
+    context = {
+        "disease" : disease,
+        "predictions" : predictions,
+        "symptoms"  : symptoms
+    }
+    return render(request, 'conditions.html', context)
+
 
 def details(request):
     return render(request, 'details.html')
+
 
 def treatment(request):
     return render(request, 'treatment.html')
@@ -30,8 +43,10 @@ def get_basic_info(request):
     if request.method=="POST":
         age = request.POST['age']
         gender= request.POST["gender"]
+
         with open('db/variables.json', 'r', encoding='utf-8') as file:
             existing_data = json.load(file)
+
         existing_data["age"] = age
         existing_data["gender"] = gender
 
@@ -39,9 +54,11 @@ def get_basic_info(request):
             json.dump(existing_data, file, indent=4, ensure_ascii=False)
     return redirect('symptoms')
 
+
 def get_symptoms(request):
     if request.method == "POST":
         symptoms = request.POST['symptoms']
+
         with open('db/variables.json', 'r', encoding='utf-8') as file:
             existing_data = json.load(file)
 
@@ -52,10 +69,12 @@ def get_symptoms(request):
           
     return redirect('questions')
 
+
 def get_other_info(request):
     if request.method == "POST":
         medications = request.POST['medications']
         conditions = request.POST['conditions']
+        
         with open('db/variables.json', 'r', encoding='utf-8') as file:
             existing_data = json.load(file)
 
@@ -64,5 +83,16 @@ def get_other_info(request):
 
         with open('db/variables.json', 'w', encoding='utf-8') as file:
             json.dump(existing_data, file, indent=4, ensure_ascii=False)
+        
+        symptoms = existing_data["symptoms"]
+        data, svm, nb, rf = doctor.prep_data()
+
+        predictions = doctor.predictDisease(symptoms, data, svm, nb, rf) 
+
+        disease = predictions["final_prediction"]
           
-    return redirect('conditions')
+    return redirect('conditions', disease = disease, predictions = predictions, symptoms = symptoms)
+
+
+    
+
